8ff7d6a6b97aec404fa80333996c31567bf16090
Ekta3012 <ekta30121995@gmail.com>
2016-05-15 02:08:24 +0530

    js added

--- js/app.js
+++ js/app.js
@@ -1,20 +1,23 @@
-var chat=document.getElementById("chatwindow");
-var msg=document.getElementById("messagebox");
+var chat = document.getElementById("chatwindow");
+var msg = document.getElementById("messagebox");
 
 var socket = new WebSocket("ws://127.0.0.1:2000");
 
-function addMessage(msg){
-	chat.InnerHTML += "<p>" + msg + "</p>";
+function addMessage(msg) {
+	chat.innerHTML += "<p>" + msg + "</p>";
 }
 
-msg.addEventListener('keypress' , function(evt) {
-	if (evt.charCode != 13 || msg.value == "") 			//charcode 13=enter key
+msg.addEventListener('keypress',function(evt) {
+	if (evt.charCode != 13) 			//charcode 13=enter key
+		return;
+	
+	if(msg.value == "")
 		return;
 
 	evt.preventDefault();
 
 	socket.send(JSON.stringify({ 				//JSON.stringify() method converts a JavaScript value to a JSON string.
-		msg: msg.value
+		msg:msg.value
 	}));
 
 	addMessage(msg.value);
